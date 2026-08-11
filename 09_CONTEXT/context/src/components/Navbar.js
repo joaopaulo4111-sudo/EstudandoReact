149c59c0392ca8c575fb07c574d86855cb1dfe03
João Paulo Castro dos Santos <joaocsantos@caesb.df.gov.br>
2026-08-11 17:25:23 -0300

    Iniciando context

---
+++ 09_CONTEXT/context/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import {Link,NavLink} from 'react-router-dom'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <NavLink to='/'>Home</NavLink>
+      <NavLink to='/products'>Products</NavLink>
+      <NavLink to='/about'>Sobre</NavLink>
+      
+    </nav>
+  )
+}
+
+export default Navbar
